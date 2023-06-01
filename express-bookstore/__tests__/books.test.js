470163d3bd83d811c795dad108dd782595ac1c5c
mego <megocapps@gmail.com>
2023-06-01 09:28:31 -0400

    changes

--- express-bookstore/__tests__/books.test.js
+++ express-bookstore/__tests__/books.test.js
@@ -1,59 +1,149 @@
-const request = require('supertest');
-const app = require('../app'); // Import your Express app
-const Book = require('../models/book');
-const db = require('../models/db');
+/** Integration tests for books route */
 
 
-// Sample book data
-const testBook = {
-  isbn: "1234567890",
-  amazon_url: "http://amazon.com/somebook",
-  author: "Test Author",
-  language: "english",
-  pages: 100,
-  publisher: "Test Publisher",
-  title: "Test Book",
-  year: 2023
-};
+process.env.NODE_ENV = "test"
 
-// Start tests
-describe('Book Routes', () => {
-  
-  // Testing GET route
-  it('should show all books', async () => {
-    const res = await request(app).get('/books');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('books');
-  });
+const request = require("supertest");
 
-  // Testing POST route
-  it('should create a new book', async () => {
-    const res = await request(app).post('/books').send(testBook);
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toHaveProperty('book');
-    expect(res.body.book).toEqual(testBook);
-  });
 
-  // Testing PUT route
-  it('should update an existing book', async () => {
-    const updatedBook = {...testBook, title: 'Updated Book'};
-    const res = await request(app).put(`/books/${testBook.isbn}`).send(updatedBook);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('book');
-    expect(res.body.book.title).toEqual(updatedBook.title);
-  });
+const app = require("../app");
+const db = require("../db");
 
-  // Testing DELETE route
-  it('should delete a book', async () => {
-    const res = await request(app).delete(`/books/${testBook.isbn}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'Book deleted');
-  });
 
-  // Assuming db is your database connection instance
-afterAll(async () => {
-    await db.$pool.end();  // this should close your database connection
-  });
-  
+// isbn of sample book
+let book_isbn;
+
+
+beforeEach(async () => {
+  let result = await db.query(`
+    INSERT INTO
+      books (isbn, amazon_url,author,language,pages,publisher,title,year)
+      VALUES(
+        '123432122',
+        'https://amazon.com/taco',
+        'Elie',
+        'English',
+        100,
+        'Nothing publishers',
+        'my first book', 2008)
+      RETURNING isbn`);
+
+  book_isbn = result.rows[0].isbn
 });
 
+
+describe("POST /books", function () {
+  test("Creates a new book", async function () {
+    const response = await request(app)
+        .post(`/books`)
+        .send({
+          isbn: '32794782',
+          amazon_url: "https://taco.com",
+          author: "mctest",
+          language: "english",
+          pages: 1000,
+          publisher: "yeah right",
+          title: "amazing times",
+          year: 2000
+        });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.book).toHaveProperty("isbn");
+  });
+
+  test("Prevents creating book without required title", async function () {
+    const response = await request(app)
+        .post(`/books`)
+        .send({year: 2000});
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+
+describe("GET /books", function () {
+  test("Gets a list of 1 book", async function () {
+    const response = await request(app).get(`/books`);
+    const books = response.body.books;
+    expect(books).toHaveLength(1);
+    expect(books[0]).toHaveProperty("isbn");
+    expect(books[0]).toHaveProperty("amazon_url");
+  });
+});
+
+
+describe("GET /books/:isbn", function () {
+  test("Gets a single book", async function () {
+    const response = await request(app)
+        .get(`/books/${book_isbn}`)
+    expect(response.body.book).toHaveProperty("isbn");
+    expect(response.body.book.isbn).toBe(book_isbn);
+  });
+
+  test("Responds with 404 if can't find book in question", async function () {
+    const response = await request(app)
+        .get(`/books/999`)
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+
+describe("PUT /books/:id", function () {
+  test("Updates a single book", async function () {
+    const response = await request(app)
+        .put(`/books/${book_isbn}`)
+        .send({
+          amazon_url: "https://taco.com",
+          author: "mctest",
+          language: "english",
+          pages: 1000,
+          publisher: "yeah right",
+          title: "UPDATED BOOK",
+          year: 2000
+        });
+    expect(response.body.book).toHaveProperty("isbn");
+    expect(response.body.book.title).toBe("UPDATED BOOK");
+  });
+
+  test("Prevents a bad book update", async function () {
+    const response = await request(app)
+        .put(`/books/${book_isbn}`)
+        .send({
+          isbn: "32794782",
+          badField: "DO NOT ADD ME!",
+          amazon_url: "https://taco.com",
+          author: "mctest",
+          language: "english",
+          pages: 1000,
+          publisher: "yeah right",
+          title: "UPDATED BOOK",
+          year: 2000
+        });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("Responds 404 if can't find book in question", async function () {
+    // delete book first
+    await request(app)
+        .delete(`/books/${book_isbn}`)
+    const response = await request(app).delete(`/books/${book_isbn}`);
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+
+describe("DELETE /books/:id", function () {
+  test("Deletes a single a book", async function () {
+    const response = await request(app)
+        .delete(`/books/${book_isbn}`)
+    expect(response.body).toEqual({message: "Book deleted"});
+  });
+});
+
+
+afterEach(async function () {
+  await db.query("DELETE FROM BOOKS");
+});
+
+
+afterAll(async function () {
+  await db.end()
+});
